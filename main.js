f91977a23c95b8059f3bcc8f405ac611736d5241
Lin Hsuan <hsuan.h.lin@gmail.com>
2020-04-10 16:38:10 +0800

    d
    d

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
-const auto = false; // Auto scroll
-const intervalTime = 5000;
+const auto = true; // Auto scroll
+const intervalTime = 4000;
 let slideInterval;
 
 const nextSlide = () => {
